db1ee498839b9509a1344e392a06b5b0fa7a6b6a
David Rigan <david.rigan@students.amikom.ac.id>
2019-11-10 22:48:44 +0700

    configure testing

--- project/config.py
+++ project/config.py
@@ -4,6 +4,7 @@ class BaseConfig:
     """Base Configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(BaseConfig):
     """Developement Configuration"""
@@ -15,7 +16,5 @@ class TestingConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProdcutionConfig(BaseConfig):
-    """Production COnfig""" 
+    """Production COnfig"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
-
